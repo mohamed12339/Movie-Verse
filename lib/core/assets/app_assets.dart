04f176017d8ee9b012fa471dab1a5f7a25110001
mohamed magdy <blood_201044@yahoo.com>
2025-08-28 04:30:39 +0300

    initialCommit

---
+++ lib/core/assets/app_assets.dart
@@ -0,0 +1,39 @@
+class AppAssets {
+  // Avatars
+  static const String avatar1 = "assets/images/avatar1.png";
+  static const String avatar2 = "assets/images/avatar2.png";
+  static const String avatar3 = "assets/images/avatar3.png";
+  static const String avatar4 = "assets/images/avatar4.png";
+  static const String avatar5 = "assets/images/avatar5.png";
+  static const String avatar6 = "assets/images/avatar6.png";
+  static const String avatar7 = "assets/images/avatar7.png";
+  static const String avatar8 = "assets/images/avatar8.png";
+  static const String avatar9 = "assets/images/avatar9.png";
+
+  // Logos
+  static const String brandingLogo = "assets/images/branding_logo.png";
+  static const String splashLogo = "assets/images/splash_logo.png";
+  static const String logo = "assets/images/logo.png";
+
+  // Icons
+  static const String icBrowse = "assets/images/ic_browse.png";
+  static const String icHome = "assets/images/ic_home.png";
+  static const String icProfile = "assets/images/ic_profile.png";
+  static const String icSearch = "assets/images/ic_search.png";
+
+  // Posters
+  static const String mainPoster = "assets/images/main_poster.png";
+  static const String poster1 = "assets/images/poster1.png";
+  static const String poster2 = "assets/images/poster2.png";
+  static const String poster3 = "assets/images/poster3.png";
+  static const String poster4 = "assets/images/poster4.png";
+  static const String poster5 = "assets/images/poster5.png";
+
+  // Buttons
+  static const String forgetPasswordBg = "assets/images/forget_password_bg.png";
+
+
+  // Backgrounds
+  static const String watchNow = "assets/images/watch_now.png";
+  static const String availableNow = "assets/images/available_now.png";
+}
---
+++ lib/core/styles/app_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+  class AppStyles{
+  static TextStyle bold36 = TextStyle(
+    fontSize: 36.0,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle bold24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle bold20 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle regular20 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular15 = TextStyle(
+    fontSize: 15.0,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400
+  );
+}
