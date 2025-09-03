cdfdba09b7e2add5ea1c433aad96c21a85883a1d
mohamed magdy <blood_201044@yahoo.com>
2025-09-03 19:51:26 +0300

    Finished still in complete but just upgrade things

--- lib/Features/movie_details/ui/cubit/movie_details_and_suggestion_state.dart
+++ lib/Features/movie_details/ui/cubit/movie_details_and_suggestion_state.dart
@@ -2,35 +2,34 @@ import '../../../../core/api_result/api_result.dart';
 import '../../domain/model/entites/movie_details_dm.dart';
 import '../../domain/model/entites/movie_suggestions_dm.dart';
 
-class MovieDetailsAndSuggestionState {
-  final ApiResult<MovieDetailsDm> movieDetailsApi;
-  final ApiResult<List<SuggestedMovieDM>> movieSuggestionApi;
 
-  MovieDetailsAndSuggestionState({required this.movieDetailsApi , required this.movieSuggestionApi});
+class MovieDetailsAndSuggestionState { /// دا الـ state اللي هيحتفظ بالـ data والحالات (Initial, Loading, Success, Error) وهابتعها لل cubit
 
-  factory MovieDetailsAndSuggestionState.initial() {
-    return MovieDetailsAndSuggestionState(movieDetailsApi: InitialApiResult() , movieSuggestionApi: InitialApiResult());
+  late ApiResult<MovieDetailsDm> movieDetailsApi; /// هنا بنخزن حالة تفاصيل الفيلم
+
+
+  late ApiResult<List<SuggestedMovieDM>> movieSuggestionApi; /// هنا بنخزن حالة الأفلام المقترحة
+
+
+  MovieDetailsAndSuggestionState({ /// constructor اساسي بياخد القيم المطلوبة
+    required this.movieDetailsApi,
+    required this.movieSuggestionApi,
+  });
+
+
+ MovieDetailsAndSuggestionState.initial() {  /// والي هابعتها لل super  (الحالة الابتدائية قبل ما نجيب أي بيانات) initial state
+      movieDetailsApi = InitialApiResult();   /// كل حاجة لسه فاضية، قبل أي تحميل
+      movieSuggestionApi = InitialApiResult();  /// كل حاجة لسه فاضية، قبل أي تحميل
   }
 
-  MovieDetailsAndSuggestionState copyWith({ApiResult<MovieDetailsDm>? movieDetailsApi , ApiResult<List<SuggestedMovieDM>>? movieSuggestionApi}) {
+
+  MovieDetailsAndSuggestionState copyWith({  /// زي ال colors لما بقولوا غير الحتة بس بلون كزا فا ال  copyWith بتسهل علينا تحديث الـ state من غير ما نغير القديم كله
+    ApiResult<MovieDetailsDm>? movieDetailsApi, /// انا هنا بقولوا الي هاتغير مين دلوقتي movieDetailsApi وممكن تبقا ب null بردوا
+    ApiResult<List<SuggestedMovieDM>>? movieSuggestionApi, /// انا هنا بقولوا الي هاتغير مين دلوقتي movieSuggestionApi وممكن تبقا ب null بردوا
+  }) {
     return MovieDetailsAndSuggestionState(
-      movieDetailsApi: movieDetailsApi ?? this.movieDetailsApi,
-      movieSuggestionApi: movieSuggestionApi ?? this.movieSuggestionApi,
+      movieDetailsApi: movieDetailsApi ?? this.movieDetailsApi,         /// لو جاب فيلم جديد استخدمها ، غير كده خلي القديم
+      movieSuggestionApi: movieSuggestionApi ?? this.movieSuggestionApi, /// نفس الفكرة للأفلام المقترحة
     );
   }
 }
-// class MovieSuggestionState {
-//   final ApiResult<List<SuggestedMovieDM>> movieSuggestionApi;
-//
-//   MovieSuggestionState({required this.movieSuggestionApi});
-//
-//   factory MovieSuggestionState.initial() {
-//     return MovieSuggestionState(movieSuggestionApi: InitialApiResult());
-//   }
-//
-//   MovieSuggestionState copyWith({ApiResult<List<SuggestedMovieDM>>? movieSuggestionApi}) {
-//     return MovieSuggestionState(
-//       movieSuggestionApi: movieSuggestionApi ?? this.movieSuggestionApi,
-//     );
-//   }
-// }
